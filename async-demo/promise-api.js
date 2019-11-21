0a4560a2089ec357077085e74e653f75fd3f8251
csjinf <csjinf@csjinfs-mbp.mynet>
2019-11-21 00:21:04 +0000

    removed async

--- async-demo/promise-api.js
+++
@@ -1,7 +0,0 @@
-
-//resolved Promises, can be used for unit testing. Look into this further
-const p = Promise.resolve({ id: 1});
-p.then(result => console.log(result)); 
-
-const pr = Promise.reject(new Error('This is teh reason for failure...'));
-pr.catch(err => console.log(err));
